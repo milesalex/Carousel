f7ccb6240af43bbaa442094dbd73996843d4e753
Alex Miles <alexmiles@dropbox.com>
2016-02-08 16:31:04 -0800

    init

---
+++ Carousel/IntroViewController.swift
@@ -0,0 +1,36 @@
+//
+//  IntroViewController.swift
+//  Carousel
+//
+//  Created by Alex Miles on 2/8/16.
+//  Copyright © 2016 Dropbox. All rights reserved.
+//
+
+import UIKit
+
+class IntroViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.scrollView.contentSize = CGSize(width: 320, height: 1136)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
